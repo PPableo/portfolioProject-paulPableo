a15ccbd7d918e7e8b1b23342d830f9bd423bc9ce
PPableo <pableop@gmail.com>
2023-01-06 15:37:27 -0500

    Added New Projects, added new CSS, Updated Project Discriptions

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,13 @@
 // NEW FEATURES COMING SOON 
 // JS RELATED
+
+// /* ---------------------- Limit Animation to 1st Visit ---------------------- */
+// window.addEventListener(('load'), () => {
+//     if (document.querySelector('.introAnimation') !== null) {
+//       window.sessionStorage.setItem('Heading', 'displayed');
+//     }
+//   })
+
+//   if (window.sessionStorage.getItem('Heading')) {
+//     document.querySelector('#heading').classList.remove('introAnimation')
+//   }
