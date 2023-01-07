c78cd48a3fd0b1e739dfde8146a1dd4f1281180a
PPableo <pableop@gmail.com>
2023-01-06 23:48:47 -0500

    Added Projects Section, Contact Modal via JS, styled, unclean, and not finalized

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,60 @@
 //   if (window.sessionStorage.getItem('Heading')) {
 //     document.querySelector('#heading').classList.remove('introAnimation')
 //   }
+
+/* ------------------------------ Active Modals ----------------------------- */
+// Get the modal
+const modal = document.querySelector('#myModal');
+const mainContent = document.querySelector('.mainContent')
+const projectPage = document.querySelector('.projectSection')
+const techStack = document.querySelector('.techStack')
+const projectList = document.querySelector('.projectsList')
+// const projectModal = document.querySelector('projectSection')
+const aboutMe = document.querySelector('.aboutME')
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn1");
+const btn2 = document.getElementById("myBtn2");
+const btn3 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+//   modal.style.display = "block";
+techStack.style.display = 'block'
+aboutMe.style.display = 'block'
+projectList.classList.remove('open')
+//   projectModal.style.display = 'block'
+}
+
+btn2.onclick = function() {
+//   modal.style.display = "block";
+techStack.style.display = 'none'
+aboutMe.style.display = 'none'
+projectList.classList.add('open')
+mainContent.style.display = 'block'
+//   projectModal.style.display = 'none'
+}
+
+btn3.onclick = function() {
+  modal.style.display = "block";
+// techStack.style.display = 'none'
+// aboutMe.style.display = 'none'
+// projectList.classList.add('open')
+// mainContent.style.display = 'block'
+//   projectModal.style.display = 'none'
+}
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
