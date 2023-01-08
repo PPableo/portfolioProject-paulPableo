8ad489b0f613fdf4d4493de0eec6c9cacbe60220
PPableo <pableop@gmail.com>
2023-01-07 23:08:39 -0500

    Fixed mediaQueries for mobile responsiveness and text

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,6 @@ const mainContent = document.querySelector('.mainContent')
 const projectPage = document.querySelector('.projectSection')
 const techStack = document.querySelector('.techStack')
 const projectList = document.querySelector('.projectsList')
-// const projectModal = document.querySelector('projectSection')
 const aboutMe = document.querySelector('.aboutME')
 // Get the button that opens the modal
 const btn = document.getElementById("myBtn1");
@@ -27,35 +26,26 @@ const btn2 = document.getElementById("myBtn2");
 const btn3 = document.getElementById("myBtn3");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-//   modal.style.display = "block";
 techStack.style.display = 'block'
 aboutMe.style.display = 'block'
 projectList.classList.remove('open')
-//   projectModal.style.display = 'block'
 }
 
 btn2.onclick = function() {
-//   modal.style.display = "block";
 techStack.style.display = 'none'
 aboutMe.style.display = 'none'
 projectList.classList.add('open')
 mainContent.style.display = 'block'
-//   projectModal.style.display = 'none'
 }
 
 btn3.onclick = function() {
   modal.style.display = "block";
-// techStack.style.display = 'none'
-// aboutMe.style.display = 'none'
-// projectList.classList.add('open')
-// mainContent.style.display = 'block'
-//   projectModal.style.display = 'none'
-}
 
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -63,8 +53,8 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
